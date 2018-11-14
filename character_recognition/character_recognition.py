780e198de259951323983f9a02b5d1b56843e56c
Hyemin Bang <hhyeminbang@gmail.com>
2018-11-14 10:02:05 -0500

    character recognition for now

--- character_recognition/character_recognition.py
+++ character_recognition/character_recognition.py
@@ -5,13 +5,14 @@ import cv2
 import os, sys
 from PIL import Image
 import pyrebase
+import base64
 
 #Configure Firebase
 config = {
-  "apiKey": "AIzaSyAHex43Nme85Xf2foycuONIU-jlR5yZYww",
-  "authDomain": "imageclassify-93207.firebaseapp.com",
-  "databaseURL": "https://imageclassify-93207.firebaseio.com/",
-  "storageBucket": "imageclassify-93207.appspot.com"
+  "apiKey": "AIzaSyDuiOnplJjjoh9poil-h67uFPUBw7ojJ0c",
+  "authDomain": "gesturekeyboard.firebaseapp.com",
+  "databaseURL": "https://gesturekeyboard.firebaseio.com",
+  "storageBucket": "gesturekeyboard.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -19,20 +20,30 @@ storage = firebase.storage()
 db = firebase.database()
 
 # get the images and their names from the database
-images = db.child("images")
+images = db.child("Data")
 
 # keep the key of the image to store classification later
 imageKey = ""
+filename = ""
 
 # store the images locally for classification
 for imageData in images.get().each():
   imageVal = imageData.val()
-  if imageVal is not None and "classification" not in imageVal.keys():
-    # downloading the image from the firebase storage
-    imageName = imageVal["imageName"]
-    storage.child(imageName).download("images/" + imageName)
-    imageKey = imageData.key()
-    break
+  if imageVal is not None:
+    if "classification" not in imageVal.keys():
+      imageKey = imageData.key() # saved for future classification
+
+      # decoding the base64 data to a png image and downloading it
+      imageString = imageVal["imgsrc"].split("base64,",1)[1]
+      imageBase64Data = base64.b64decode(imageString)
+
+      # downloading the image from the firebase storage
+      filename = "images/" + imageVal['imgname']
+
+      with open(filename, 'wb') as f:
+        f.write(imageBase64Data)
+
+      break
 
 # Load the models built in the previous steps
 cnn_model = load_model('emnist_cnn_model.h5')
@@ -45,31 +56,26 @@ letters = { 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: '
 # Define a 5x5 kernel for erosion and dilation
 kernel = np.ones((5, 5), np.uint8)
 
-# Define prediction variables
-prediction1 = 26
-prediction2 = 26
+# # Define prediction variable
+prediction = 26
 
-all_images = []
-for image_path in os.listdir('images/'):
-    if "png" in str(image_path):
-        image_path = 'images/' + image_path
+# if there is a letter to be classified
+if (len(filename) > 0):
+  img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        # print(img)
-        # im_array = cv2.bitwise_not(img)
+  newImage = cv2.resize(img, (28, 28))
+  print(newImage)
+  newImage = np.array(newImage)
+  newImage = newImage.astype('float32')/255
 
-        newImage = cv2.resize(img, (28, 28))
-        print(newImage)
-        newImage = np.array(newImage)
-        newImage = newImage.astype('float32')/255
+  prediction = cnn_model.predict(newImage.reshape(1,28,28,1))[0]
+  prediction = np.argmax(prediction)
 
-        prediction2 = cnn_model.predict(newImage.reshape(1,28,28,1))[0]
-        prediction2 = np.argmax(prediction2)
+  # print("Convolution Neural Network:  " + str(letters[int(prediction)+1]), (10, 440), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-print("Convolution Neural Network:  " + str(letters[int(prediction2)+1]), (10, 440), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+  # adding the classified letter to the database
+  classification = str(letters[int(prediction)+1])
+  db.child("Data").child(imageKey).update({"classification" : classification})
 
-# adding the classified letter to the database
-classification = str(letters[int(prediction2)+1])
-db.child("images").child(imageKey).update({"classification" : classification})
 
 
